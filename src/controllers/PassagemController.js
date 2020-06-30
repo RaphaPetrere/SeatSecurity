e0d8ebd9e2734142c4467cc50d305004cf7218bf
RaphaPetrere <raphaelpetrere@gmail.com>
2020-06-30 18:42:28 -0300

    [UPDATE] Adicionando dados do veiculo

--- src/controllers/PassagemController.js
+++ src/controllers/PassagemController.js
@@ -23,9 +23,22 @@ module.exports = {
         .first();
         
         let motoristas = ["Chen", "Mateus", "Luiz", "Sunny", "Jennie", "Nako", "Mr.SM", "Yang Hyunsuk", "JYP - The Asian Soul"];
+        let carros = [{
+            veiculo : "Mercedes-Benz 413 Van", cor : "Cinza", placa : "ABC-1234"},
+            {veiculo : "Ford Transit", cor : "Azul", placa : "HSS-4938"},
+            {veiculo : "Citroën Jumpy Minibus", cor : "Branco", placa : "MZC-6288"},
+            {veiculo : "Peugeot Expert Minibus", cor : "Gelo", placa : "GPQ-5880"},
+            {veiculo : "Mercedes Sprinter 415 Van", cor : "Verde", placa : "NAF-5383"},
+            {veiculo : "Fiat Ducato Minibus Comfort", cor : "Vinho", placa : "NEM-7145"},
+            {veiculo : "Renault Master Minibus", cor : "Branco", placa : "JYZ-2672"},
+            {veiculo : "Mercedes-Benz Sprinter 313", cor : "Preto", placa : "MWZ-9040"},
+            {veiculo : "Peugeot Boxer", cor : "Cinza", placa : "MGK-1121"},
+            {veiculo : "Fiat Ducato Combinato", cor : "Vinho", placa : "JUZ-7938"
+        }];
         let numero = Math.floor(Math.random() * 10);
         console.log(numero);
-        let escolhido = motoristas[numero];
+        let motoristaEscolhido = motoristas[numero];
+        let veiculoEscolhido = carros[numero];
         try
         {
             transporter.sendMail({
@@ -38,7 +51,10 @@ module.exports = {
                 Data e hora : ${viagem.data} - ${viagem.hora}
                 Quantidade de Pessoas: ${viagem.qtdPessoas}
                 Preço: ${viagem.preco}
-                Motorista: ${escolhido}.
+                Motorista: ${motoristaEscolhido}
+                Veiculo: ${veiculoEscolhido.veiculo}
+                Cor: ${veiculoEscolhido.cor}
+                Placa: ${veiculoEscolhido.placa}.
                 A equipe da Seat Security agradece a preferencia, faça uma boa viagem e volte sempre!`,
             }).then(message => {
                 console.log(message);
